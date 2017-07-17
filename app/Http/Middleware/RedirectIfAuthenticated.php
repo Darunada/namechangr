cd677adba6cb75c7b014f0765e3d6d67d41badae
Lea Fairbanks <lea.rae.fairbanks@gmail.com>
2017-07-16 21:01:30 -0600

    fix little bug with redirecting home from login or forgot password page going to 404

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Support\Facades\Auth;
+use Laracasts\Flash\Flash;
 
 class RedirectIfAuthenticated
 {
@@ -18,7 +19,8 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            return redirect('home');
+            Flash::info("You are already logged in!");
+            return redirect()->route('dashboard');
         }
 
         return $next($request);
